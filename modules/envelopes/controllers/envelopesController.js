3485c9e17bbb5f29bcb525c9538e1ea5f192b103
Kevin Cain <kevinmcain@hotmail.com>
2015-05-10 22:53:58 -0700

    rm code

--- modules/envelopes/controllers/envelopesController.js
+++ modules/envelopes/controllers/envelopesController.js
@@ -8,30 +8,24 @@
  *
  * Main module of the application.
  */
-angular
-.module('budgyApp')
-.controller('EnvelopesCtrl', ['$scope', '$rootScope', '$http',
-	function($scope,  $rootScope, $http) {
-		$scope.getEnvelopes = function() {
+(function(){
 
-			var url = '/envelopes/' + $rootScope.budgetId;
-			$http.get(url).success(function(data, status, headers, config) {
-			   $scope.results = data;
-			   var envelopes = [];
-			   for (var i = 0; i < data.length; i++) {
-				   envelopes[i] = 
-					{ "category": data[i].category, "balance": data[i].balance };
-			   }
-			   $rootScope.envelopes = envelopes;
-			});
-		};
-		// I dont think we'll need a write function here. we'll probably need 
-		// this in the manage envelopes controller
-		$scope.writeEnvelopes = function() {
-			console.log($scope.list);
-			$http.post('/envelopes', $scope.envelopes).success(function(data, status, headers, config) {
-				$scope.list.listId = data;
-			});
-		};
-	}
-]);
+	var app = angular.module('budgyApp');
+
+	app.controller('EnvelopesCtrl', [
+		'$scope', 
+		'$rootScope',
+		'$http', 
+		function($scope,  $rootScope, $http) {
+		
+			$scope.getEnvelopes = function() {
+
+				var url = '/envelopes/' + $rootScope.budgetId;
+			
+				$http.get(url).success(function(data, status, headers, config) {
+					$scope.envelopes = data;
+				});
+			};
+		}
+	]);
+})();
